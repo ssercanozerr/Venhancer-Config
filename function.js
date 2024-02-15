3f767518e82bad0f983fb39b4bd00ed42849a57b
Sercan <ssercanozerr@gmail.com>
2024-02-15 16:01:52 +0300

    abc

--- function.js
+++ function.js
@@ -9,8 +9,8 @@ function TaskManager() {
       const taskIndex = this.tasks.findIndex(task => task.name === taskName);
       if (taskIndex !== -1) {
         this.tasks[taskIndex].completed = true;
-        console.log("dell")
-        console.log("monster")
+        console.log("bmw")
+        console.log("ford")
       } else {
         console.log("Task not found!");
       }
